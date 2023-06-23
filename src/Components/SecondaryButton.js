c8357bee05b4b7b7d7024e740796ad5312a2789d
BittyLitten <137287284+BittyLitten@users.noreply.github.com>
2023-06-23 09:22:29 +0000

    Secondsry button

--- src/Components/SecondaryButton.js
+++ src/Components/SecondaryButton.js
@@ -3,18 +3,23 @@ import styled from "styled-components";
 
 function SecondaryButton(props) {
     // Здесь устанавливается минимальная ширина для кнопки
-    let minWidth = props.minWidth === undefined ? '144px' : props.minWidth;
+    let minWidth = props.minWidth === undefined ? '217px' : props.minWidth;
     const Button = styled.div`
-        background: #F2F2F2;
-        border: 1px solid #DFDFDF;
+       
+        border: 1px solid #64748B;
         box-shadow: inset 0px 1px 2px rgba(10, 15, 26, 0.08);
         min-width: ${minWidth};
-        color: #152842;
+        border-radius: 24px;
+        color: #64748B;
         &:hover {
-            background: linear-gradient(180deg, #DFEAF1 0%, #CFDDE8 100%);
+            background: #F4F6FA;
+            border: 1px solid #F4F6FA;
+            box-shadow: none;
+        
         }
         &:active {
-            background: #DFDFDF;
+            background: #ECF1F5;
+            color: #96A3B3;
         }
     `;
     return (
